fea575a62d534512b11138324441fdec6710c131
Pokemon: The Lost Origins <theindiananimatoralt@gmail.com>
2024-02-11 14:19:44 -0500

    chage standalone

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import http.server
 import socketserver
+import os
 import webbrowser
 
 PORT = 8000
@@ -8,5 +9,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("Serving at port", PORT)
-    webbrowser.open(f"http://localhost:{PORT}")
+    os.system(f"start http://localhost:{PORT}")
     httpd.serve_forever()
